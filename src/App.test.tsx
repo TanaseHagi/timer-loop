05d2724e69fad97f05be35f8644ce62c7bd5a89b
Tănase Hagi <tanase.hagi@gmail.com>
2018-05-22 19:08:50 +0300

    added test for App.getPreviousTimer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import App, { IAppState } from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("getPreviousTimer", () => {
+  const state = App.getInitialState();
+  const length = state.timers.length;
+  
+  state.currentTimerIndex = 0;
+  let previous = App.getPreviousTimer(state);
+  expect(previous).toEqual(length - 1);
+  
+  state.currentTimerIndex = 1;
+  previous = App.getPreviousTimer(state);
+  expect(previous).toEqual(0);
 });
